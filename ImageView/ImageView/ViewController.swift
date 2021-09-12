402664813f872849fb71175652eb85a2c8e60bba
soyunim <43322159+soyunim@users.noreply.github.com>
2021-09-12 22:18:43 +0900

    modified

--- ImageView/ImageView/ViewController.swift
+++ ImageView/ImageView/ViewController.swift
@@ -18,12 +18,39 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imgOn = UIImage(named: "lamp_on.jpeg")
+        imgOff = UIImage(named: "lamp_off.jpeg")
+        
+        imgView.image = imgOn
     }
 
     @IBAction func btnResizeImage(_ sender: UIButton) {
+        let scale:CGFloat = 2.0
+        var newWidth:CGFloat, newHeight:CGFloat
+        
+        if (isZoom) {    // true
+            newWidth = imgView.frame.width/scale
+            newHeight = imgView.frame.height/scale
+            btnResize.setTitle("확대", for: .normal)
+        }
+        else {  // false
+            newWidth = imgView.frame.width*scale
+            newHeight = imgView.frame.height*scale
+            btnResize.setTitle("축소", for: .normal)
+            
+        }
+        imgView.frame.size = CGSize(width: newWidth, height: newHeight)
+        isZoom = !isZoom
     }
     
     @IBAction func switchImageOnoff(_ sender: UISwitch) {
+        if sender.isOn {
+            imgView.image = imgOn
+        }
+        else {
+            imgView.image = imgOff
+        }
     }
 }
 
